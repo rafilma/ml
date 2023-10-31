1018254ed75949a96ce77d74106845c70bf80bbb
rafilma <148635738+rafilma@users.noreply.github.com>
2023-10-31 19:25:21 +0700

    Update stream-cancer.py

--- stream-cancer.py
+++ stream-cancer.py
@@ -12,8 +12,7 @@ if Gender == 'Laki laki':
     Gender = 1
 else:
     Gender = 2
-Air_Pollution = st.number_input(
-    'Masukkan konsentrasi serum albumin pasien.', min_value=1, max_value=10, step=0.1)
+Air_Pollution = st.text_input('Dalam skala 1-10 seberapa sering anda terpapar polusi udara ?')
 Alcohol_use = st.text_input('Dalam skala 1-10 seberapa sering anda meminum alkohol ?')
 Dust_Allergy = st.text_input('Dalam skala 1-10 apakah anda punya alergi terhadap debu ?')
 OccuPational_Hazards = st.text_input('Dalam skala 1-10 apakah pekerjaan anda melibatkan bahan berbahaya ?')
